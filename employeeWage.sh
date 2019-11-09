18c63a074221ea0720dfbdfe43a3276ba3e28f83
usahu998 <usahu998@gmail.com>
2019-11-09 17:38:00 +0530

    added part time feature

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,14 +1,14 @@
 #! /bin/bash -x
 
-isPresent=1
+isFullTime=1
+isPartTime=2
 hourlyWage=30
 empCheck=$((RANDOM%2))
-if [ $empCheck == $isPresent ] 
+if [ $empCheck == $isFullTime ] 
 then 
 	hours=8
-	wage=$(($hourlyWage*hours))
-	echo emp present
-else
-	echo emp absent
+elseif [ $empCheck == $isPartTime ]
+	hours=4
 fi
-	
+wage=$(($hourlyWage*hours))
+
