cac3457f972d58eed4789409c39c35fbc449a742
usahu998 <usahu998@gmail.com>
2019-11-09 18:55:46 +0530

    conflict check

--- employeeWage.sh
+++ employeeWage.sh
@@ -6,9 +6,11 @@ hourlyWage=30
 empCheck=$((RANDOM%2))
 if [ $empCheck == $isFullTime ] 
 then 
-	hoursWorked=8
-elseif [ $empCheck == $isPartTime ]
+	hours=8
+elif [ $empCheck == $isPartTime ]
 then
 	hours=4
+else
+	hours=0
 fi
-wage=$(($hourlyWage*hours))
+wage=$(($hourlyWage*$hours))
