247d064948145a296147ec2d4fe22f7518926a53
Ayush Saraf <ayush.saraf47@gmail.com>
2019-11-09 17:19:25 +0530

    added daily wage calculations

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,12 @@
 #! /bin/bash -x
 
 isPresent=1
+hourlyWage=30
 empCheck=$((RANDOM%2))
 if [ $empCheck == $isPresent ] 
 then 
+	hours=8
+	wage=$(($hourlyWage*hours))
 	echo emp present
 else
 	echo emp absent
